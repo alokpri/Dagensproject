21b7001010450b246c278f075958aecac1ce77a0
alokpri <157834365+alokpri@users.noreply.github.com>
2024-01-26 20:25:26 +0100

    Update products.js
    formetted

--- server/products.js
+++ server/products.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const db = require('./db');
 
-
-
-
 router.get('/', (req, res) => {
   // Retrieve query parameters
   const { category, minPrice, maxPrice, page = 1 } = req.query;
